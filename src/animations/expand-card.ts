167cffc31332a6ac3f569bf5e956946815d0f580
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2020-11-17 23:28:02 +0100

    Refine expand card animation

--- src/animations/expand-card.ts
+++ src/animations/expand-card.ts
@@ -1,21 +1,32 @@
-import gsap, { Power3 } from "gsap";
+import gsap, { Power3, Power2 } from "gsap";
 
 export default ({ element, duration, delay, onComplete }) => {
   return gsap
-    .timeline()
-    .to(element, {
-      transformOrigin: "center",
-      width: "80vw"
-    })
-    .to(element, {
-      height: "70vh",
-      position: "absolute",
-
-      top: "50%",
-      left: "50%",
-      y: "-50%",
-      x: "-50%",
-
+    .timeline({
       onComplete: onComplete
-    });
+    })
+    .to(
+      element,
+      {
+        height: "70vh",
+        width: "80vw",
+        zIndex: 10,
+        ease: Power3.easeOut
+      },
+      0
+    )
+    .to(
+      element,
+      {
+        position: "absolute",
+
+        top: "50%",
+        left: "50%",
+
+        y: "-50%",
+        x: "-50%",
+        ease: Power2.easeOut
+      },
+      0
+    );
 };
