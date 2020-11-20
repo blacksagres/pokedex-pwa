06ab80f01aa79f404637e3f20134d79c2d4c6749
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2020-11-20 11:24:26 +0100

    Move styled components to its own file

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -3,6 +3,7 @@ import { useAsync } from "react-use";
 import { styled } from "../../stitches.config";
 import { bounce as bounceSprite } from "../animations";
 import { fetchPokemon } from "../gateways/poke-gateway";
+import { Card, CardHeader, CardContent } from "./containers/StyledCard";
 import { SwordAndShieldLoader } from "./SwordShieldLoader";
 import { TypeTag } from "./TypeTag";
 
@@ -12,56 +13,6 @@ const PokeSprite = styled("img", {
 
   md: {
     height: "8rem"
-  }
-});
-
-const Card = styled("div", {
-  borderRadius: "$card",
-
-  transition: "box-shadow .150s ease-in-out",
-  boxShadow: ".25rem .25rem .25rem 1px rgba(0, 0, 0, .2)",
-
-  backgroundColor: "$card-bg",
-  cursor: "pointer",
-
-  ":hover": {
-    boxShadow: ".25rem .25rem .75rem 1px rgba(0, 0, 0, .3)"
-  },
-
-  // When in row mode
-  ":not(:last-child)": {
-    marginBottom: "1rem"
-  },
-
-  // When in grid mode
-  md: {
-    ":not(:last-child)": {
-      marginBottom: 0
-    }
-  }
-});
-
-const CardContent = styled("div", {
-  display: "flex",
-  flexDirection: "row",
-  alignContent: "center",
-  justifyContent: "space-between",
-  padding: "0 1rem"
-});
-
-const CardHeader = styled("div", {
-  textAlign: "center",
-  textTransform: "capitalize",
-  fontWeight: "bold",
-
-  padding: ".3rem 0",
-
-  backgroundColor: "silver",
-  borderTopLeftRadius: "1rem",
-  borderTopRightRadius: "1rem",
-
-  md: {
-    padding: ".5rem 0"
   }
 });
 
---
+++ src/components/containers/StyledCard.tsx
@@ -0,0 +1,51 @@
+import { styled } from "../../../stitches.config";
+
+export const Card = styled("div", {
+  borderRadius: "$card",
+
+  transition: "box-shadow .150s ease-in-out",
+  boxShadow: ".25rem .25rem .25rem 1px rgba(0, 0, 0, .2)",
+
+  backgroundColor: "$card-bg",
+  cursor: "pointer",
+
+  ":hover": {
+    boxShadow: ".25rem .25rem .75rem 1px rgba(0, 0, 0, .3)"
+  },
+
+  // When in row mode
+  ":not(:last-child)": {
+    marginBottom: "1rem"
+  },
+
+  // When in grid mode
+  md: {
+    ":not(:last-child)": {
+      marginBottom: 0
+    }
+  }
+});
+
+export const CardContent = styled("div", {
+  display: "flex",
+  flexDirection: "row",
+  alignContent: "center",
+  justifyContent: "space-between",
+  padding: "0 1rem"
+});
+
+export const CardHeader = styled("div", {
+  textAlign: "center",
+  textTransform: "capitalize",
+  fontWeight: "bold",
+
+  padding: ".3rem 0",
+
+  backgroundColor: "silver",
+  borderTopLeftRadius: "1rem",
+  borderTopRightRadius: "1rem",
+
+  md: {
+    padding: ".5rem 0"
+  }
+});
