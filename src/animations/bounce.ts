5a0ad891a30ffba82289fee7e5551a91f74605e4
Geraldo dos Santos <geraldo.nogueirabs@gmail.com>
2020-11-23 17:18:44 +0100

    Add more weights to the font

--- src/animations/bounce.ts
+++ src/animations/bounce.ts
@@ -1,19 +1,17 @@
 import gsap, { Bounce } from "gsap";
 
 export default ({ element }) => {
-  return gsap
-    .fromTo(
-      element,
-      {
-        ease: Bounce.easeIn,
-        y: -5,
-        duration: 0.3
-      },
-      {
-        ease: Bounce.easeOut,
-        y: 0,
-        duration: 0.8
-      }
-    )
-    .repeat(-1);
+  return gsap.fromTo(
+    element,
+    {
+      ease: Bounce.easeIn,
+      y: -5,
+      duration: 0.3
+    },
+    {
+      ease: Bounce.easeOut,
+      y: 0,
+      duration: 0.8
+    }
+  );
 };
