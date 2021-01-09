6a4737c5c1b5608a144902a5d02c1e88f88f3089
Geraldo Santos <geraldo.santos@habtic.com>
2021-01-09 15:01:34 +0100

    Simplify close dialog

--- src/components/PokeDialog.tsx
+++ src/components/PokeDialog.tsx
@@ -1,38 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { AnimatePresence, motion } from "framer-motion";
-import { styled } from "../stitches.config";
-import { PokeData } from "../definitions";
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import { styled } from '../stitches.config';
+import type { PokeData } from '../definitions';
 import {
   GameCard,
   GameCardHeader,
   GameCardContent,
   GameCardImage,
-  GameCardInfo
-} from "./containers/StyledGameCard";
-import { TypeTagSwordAndShield } from "./TypeTagSwordShield";
+  GameCardInfo,
+} from './containers/StyledGameCard';
+import { TypeTagSwordAndShield } from './TypeTagSwordShield';
+import { useClickAway } from 'react-use';
 
 const Overlay = styled(motion.div, {
-  position: "fixed",
-  top: "0",
-  left: "0",
-  display: "block",
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  display: 'block',
 
-  height: "100vh",
-  width: "100vw",
+  height: '100vh',
+  width: '100vw',
 
-  backgroundColor: "rgba(0, 0, 0, .6)"
+  backgroundColor: 'rgba(0, 0, 0, .6)',
 });
 
 const PokeDialogContainer = styled(motion.div, {
-  margin: "3rem auto",
-  backgroundColor: "$card-bg",
-  maxWidth: "20rem",
-  borderRadius: "$card",
+  margin: '3rem auto',
+  backgroundColor: '$card-bg',
+  maxWidth: '20rem',
+  borderRadius: '$card',
 
   md: {
-    maxWidth: "40rem"
-  }
+    maxWidth: '40rem',
+  },
 });
 
 interface IPokeDialogProps {
@@ -41,25 +42,29 @@ interface IPokeDialogProps {
   pokemonData: PokeData;
 }
 
-export const PokeDialog: React.FC<IPokeDialogProps> = (props) => {
-  const { isOpen, close, pokemonData } = props;
+export const PokeDialog = (props: IPokeDialogProps) => {
+  const { close, pokemonData } = props;
+  const dialogRef = useRef(null);
+
+  useClickAway(dialogRef, close);
+
   if (!pokemonData) return null;
 
   return ReactDOM.createPortal(
     <AnimatePresence>
-      isOpen && (
       <Overlay animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
         <PokeDialogContainer
           animate={{ opacity: 1, y: 50 }}
           exit={{ opacity: 0, y: 0 }}
+          ref={dialogRef}
         >
           <GameCard data-label="game-card">
             <GameCardHeader>
               #{pokemonData.id} {pokemonData.name}
               <button
                 style={{
-                  position: "absolute",
-                  right: 10
+                  position: 'absolute',
+                  right: 10,
                 }}
                 onClick={close}
               >
@@ -83,8 +88,7 @@ export const PokeDialog: React.FC<IPokeDialogProps> = (props) => {
           </GameCard>
         </PokeDialogContainer>
       </Overlay>
-      )
     </AnimatePresence>,
-    document.querySelector("#modal-root")
+    document.querySelector('body') as HTMLBodyElement
   );
 };
