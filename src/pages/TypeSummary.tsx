38fcd6b4263dd826805f03bedd9797f05df0fe54
Kim <kim.bertrand@live.com>
2021-01-16 12:13:16 +0100

    Added dynamic type header

--- src/pages/TypeSummary.tsx
+++ src/pages/TypeSummary.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import {GameCard, GameCardContent, GameCardHeader } from '../components/containers/StyledGameCard';
+import './TypeSummary.scss'
 
 
 export const TypeSummary = (): React.ReactElement => {
+    const { type } = useParams();
 
     return (
         <GameCard data-label="game-card">
             <GameCardHeader>
+                {type}
             </GameCardHeader>
             <GameCardContent>
+                <div className="strengths">
+
+                </div>
+                <div className="weaknesses">
+
+                </div>
             </GameCardContent>
         </GameCard>
     );
