6de3fc5d9a66c856a1368c142cc6dcf4a71bcafb
Geraldo Santos <geraldo.santos@habtic.com>
2021-01-17 11:55:33 +0100

    Add build to git ignore and add letter spacing to search input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -4,7 +4,7 @@ import { CustomInput } from './CustomInput.styles';
 export const SearchInput = () => {
     return (
         <div style={{margin: '1rem 1rem 0 1rem'}}>
-            <CustomInput placeholder="🔎 Search for a pokemon..." />
+            <CustomInput css={{letterSpacing: '.1rem'}} placeholder="🔎 Search for a pokemon..." />
         </div>
     )
 }
