83aa604432673d54db4510531a6684ab782208a0
Geraldo Santos <geraldo.santos@habtic.com>
2021-01-18 08:27:15 +0100

    Add page container to handle padding and transitions in/out

--- src/pages/PokemonSummary.tsx
+++ src/pages/PokemonSummary.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import type { DoubleDamageFrom, DoubleDamageTo } from 'src/definitions/PokemonType';
+import type {
+  DoubleDamageFrom,
+  DoubleDamageTo,
+} from 'src/definitions/PokemonType';
 import { TranslateDown } from '../components/animated-transitions/TranslateDown';
 import {
   SummaryBlock,
@@ -12,10 +15,11 @@ import {
 import { TypeTagSwordAndShield } from '../components/TypeTag/TypeTagSwordShield';
 import type { CombinedPokemonData } from '../definitions/CombinedPokemonData';
 import { fetchEnrichedPokeData } from '../gateways/poke-gateway';
+import { PageContainer } from './PageContainer.styles';
 
 export const PokemonSummary = () => {
   const [pokemonData, setPokemonData] = useState<CombinedPokemonData | null>();
-  const { pokemon } = useParams<{pokemon: string}>();
+  const { pokemon } = useParams<{ pokemon: string }>();
 
   useEffect(() => {
     console.log(pokemon);
@@ -28,9 +32,9 @@ export const PokemonSummary = () => {
   if (!pokemonData) return null;
   console.log('HOI', pokemonData);
   return (
-    <>
+    <PageContainer exit={{ opacity: 0 }} animate={{ opacity: 1 }}>
       {/* <TranslateDown exit={{ height: '100vh' }} animate={{ height: '0' }} /> */}
-      <SummaryBlock exit={{ opacity: 0 }} animate={{ opacity: 1 }} data-label="game-card">
+      <SummaryBlock data-label="game-card">
         <SummaryBlockHeader>
           #{pokemonData.Pokemon.id} {pokemonData.Pokemon.name}
         </SummaryBlockHeader>
@@ -40,14 +44,17 @@ export const PokemonSummary = () => {
             alt={pokemonData.Pokemon.name}
           />
           <SummaryBlockInfo>
-            <TypeTagSwordAndShield mode="row" types={pokemonData.Pokemon.types} />
+            <TypeTagSwordAndShield
+              mode="row"
+              types={pokemonData.Pokemon.types}
+            />
             <h3>Strong versus</h3>
             <TypeTagSwordAndShield
               mode="row"
               types={pokemonData.PokeTypes.map((pokeType) =>
                 pokeType.damage_relations.double_damage_to.map(
                   (typeDoubleDamage: DoubleDamageTo) => ({
-                  type: { name: typeDoubleDamage.name }
+                    type: { name: typeDoubleDamage.name },
                   })
                 )
               ).flat()}
@@ -58,7 +65,7 @@ export const PokemonSummary = () => {
               types={pokemonData.PokeTypes.map((pokeType) =>
                 pokeType.damage_relations.double_damage_from.map(
                   (typeDoubleDamage: DoubleDamageFrom) => ({
-                    type: { name: typeDoubleDamage.name }
+                    type: { name: typeDoubleDamage.name },
                   })
                 )
               ).flat()}
@@ -66,6 +73,6 @@ export const PokemonSummary = () => {
           </SummaryBlockInfo>
         </SummaryBlockContent>
       </SummaryBlock>
-    </>
+    </PageContainer>
   );
 };
