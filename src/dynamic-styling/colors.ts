bfd66202acbe566b263d3a97bb97baaeaad26790
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-01-19 14:48:47 +0100

    Update other colors based on icons

--- src/dynamic-styling/colors.ts
+++ src/dynamic-styling/colors.ts
@@ -4,22 +4,22 @@ interface ColorDictionary {
 
 export const typeColors: ColorDictionary = {
   normal: '#A8A77A',
-  fire: '#EE8130',
+  fire: '#ff9e54',
   water: '#6390F0',
   electric: '#F7D02C',
   grass: '#7AC74C',
   ice: '#96D9D6',
-  fighting: '#C22E28',
+  fighting: '#d03e69',
   poison: '#A33EA1',
-  ground: '#E2BF65',
-  flying: '#A98FF3',
+  ground: '#dc7743',
+  flying: '#8fabe0',
   psychic: '#F95587',
   bug: '#A6B91A',
-  rock: '#B6A136',
+  rock: '#c8b88b',
   ghost: '#735797',
   dragon: '#026dc9',
   dark: '#705746',
-  steel: '#B7B7CE',
+  steel: '#5990a3',
   fairy: '#D685AD',
 };
 
