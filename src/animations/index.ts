d76e5b64d28807de6c9be2f63bfbca990422b2cc
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-01-19 18:07:47 +0100

    Remove gsap dependencies

--- src/animations/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as infiniteRotation } from "./infinite-rotation";
-export { default as bounce } from "./bounce";
-export { default as expandCard } from "./expand-card";
--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -1,39 +1,41 @@
 import React from 'react';
 import type { CombinedPokemonData } from '@definitions/CombinedPokemonData';
-import { bounce as bounceSprite } from '../../animations';
 import { Card, CardHeader, CardContent } from '../containers/StyledCard';
 import { TypeTagSwordAndShield } from '../TypeTag/TypeTagSwordShield';
 import { PokeSprite } from './PokeSprite.styles';
 
 type PokeCardProps = {
-  pokemonData: CombinedPokemonData,
+  pokemonData: CombinedPokemonData;
   onClick: (event: any) => void;
   custom: any;
   initial: any;
   animate: any;
   variants: any;
-}
+};
 
-export const PokeCard = ({ pokemonData, onClick, custom, initial, animate, variants }: PokeCardProps) => {
-  const animateSprite = (element: HTMLImageElement) => {
-    if (!element) return;
-    element.addEventListener('mouseenter', () => {
-      bounceSprite({ element: element });
-    });
-
-    element.addEventListener('click', () => {
-      bounceSprite({ element: element });
-    });
-  };
-
+export const PokeCard = ({
+  pokemonData,
+  onClick,
+  custom,
+  initial,
+  animate,
+  variants,
+}: PokeCardProps) => {
   return (
-    <Card custom={custom} initial={initial} animate={animate} variants={variants} id={pokemonData.Pokemon.name} layoutId={pokemonData.Pokemon.name} onClick={onClick}>
+    <Card
+      custom={custom}
+      initial={initial}
+      animate={animate}
+      variants={variants}
+      id={pokemonData.Pokemon.name}
+      layoutId={pokemonData.Pokemon.name}
+      onClick={onClick}
+    >
       <CardHeader>
         #{pokemonData.Pokemon.id} {pokemonData.Pokemon.name}
       </CardHeader>
       <CardContent>
         <PokeSprite
-          ref={animateSprite}
           src={pokemonData.Pokemon.sprites.front_default.toString()}
           alt="front_default"
         />
