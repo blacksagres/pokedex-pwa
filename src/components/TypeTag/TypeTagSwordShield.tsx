98e6cec866c0c483fa7872cdd4eaab1002d75081
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-01-19 10:35:31 +0100

    Animate tags container and add types for short id

--- src/components/TypeTag/TypeTagSwordShield.tsx
+++ src/components/TypeTag/TypeTagSwordShield.tsx
@@ -16,19 +16,37 @@ type TypeTagSwordShieldProps = {
 export const TypeTagSwordAndShield = ({ types, mode }: TypeTagSwordShieldProps) => {
   const history = useHistory();
 
-  const getTypeTag = (type: TypeOnFullPokemon) => {
-
-    return <Tag css={{
-      border: `.1rem solid ${typeColors[type.type.name]}`,
-      backgroundColor: hexToRgbA(typeColors[type.type.name]),
-
-      ":hover": {
-        background: typeColors[type.type.name],
+  // TODO: animate everytime?
+  // https://www.framer.com/api/motion/animation
+  const variants = {
+    toTheLeft: (index: number) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: index * 0.08,
       },
-    }} key={`type.type.name-${generateNewId()}`} onClick={(event: any) => {
-      event.stopPropagation();
-      history.push(`/type-summary/${type.type.name}`);
-    }}>
+    }),
+    righty: { x: '10px', opacity: 0 },
+  }
+
+  const getTypeTag = (type: TypeOnFullPokemon, index: number) => {
+
+    return <Tag
+      animate="toTheLeft"
+      initial="righty"
+      custom={index}
+      variants={variants}
+      css={{
+        border: `.1rem solid ${typeColors[type.type.name]}`,
+        backgroundColor: hexToRgbA(typeColors[type.type.name]),
+
+        ":hover": {
+          background: typeColors[type.type.name],
+        },
+      }} key={`type.type.name-${generateNewId()}`} onClick={(event: any) => {
+        event.stopPropagation();
+        history.push(`/type-summary/${type.type.name}`);
+      }}>
       <img src={(allTypes[type.type.name])} alt={`type-icon-${type.type.name}`} />
       <span>{type.type.name}</span>
     </Tag>;
