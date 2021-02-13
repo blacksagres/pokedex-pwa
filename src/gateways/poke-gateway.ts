388e5c77e4e42bdb81e0984b3335ddba4d30b4cf
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-02-13 16:09:21 +0100

    Use cache api to avoid reloading data live everytime

--- src/gateways/poke-gateway.ts
+++ src/gateways/poke-gateway.ts
@@ -11,80 +11,60 @@ export type TrimmedPokemonData = Pick<
   'name' | 'types' | 'id' | 'sprites'
 >;
 
-export const fetchAllPokemonNames = async (): Promise<string[]> => {
-  const pokeCache = await caches.open('poke-cache');
-  const requestUrl = `${endpoint.url}pokemon/?limit=450`;
+const cachedFetchApi = async <T>(endpoint: string): Promise<T> => {
+  try {
+    const pokeCache = await caches.open('poke-cache');
 
-  const cachedPokeNames = await pokeCache.match(
-    `${endpoint.url}pokemon/?limit=450`
-  );
+    const cachedResult = await pokeCache.match(endpoint);
 
-  if (cachedPokeNames) {
-    const jsonCachedResult = await cachedPokeNames.json();
-    return jsonCachedResult.results.map((pkmn: any) => pkmn.name);
+    if (cachedResult) {
+      const jsonCachedResult = (await cachedResult.json()) as T;
+      return jsonCachedResult;
+    }
+
+    const apiResponse = await fetch(endpoint);
+    // https://stackoverflow.com/q/45618984
+    // TL;DR - A request is a stream and can only be consumed once.
+    // Since we are consuming this once by cache and once by the browser for fetch,
+    // we need to clone the response.
+    pokeCache.put(endpoint, apiResponse.clone());
+
+    const jsonResult: T = await apiResponse.json();
+
+    return jsonResult;
+  } catch (error) {
+    console.error({ endpoint, error });
+    return {} as T;
   }
-
-  const apiResponse = await fetch(`${endpoint.url}pokemon/?limit=450`);
-  pokeCache.put(requestUrl, apiResponse);
-
-  const jsonResult = await apiResponse.json();
-  return jsonResult.results.map((pkmn: any) => pkmn.name);
 };
 
-export const fetchPokemonForOverview = ({
-  pokemonName,
-}: {
-  pokemonName: string;
-}): Promise<TrimmedPokemonData> => {
-  const cachedPokemon = getCachedObject<TrimmedPokemonData>(
-    `trimmed-data-${pokemonName}`
-  );
-
-  // console.log('fetching for overview', cachedPokemon);
-
-  if (cachedPokemon) return Promise.resolve(cachedPokemon);
-
-  return fetch(`${endpoint.url}pokemon/${pokemonName}`)
-    .then((result) => result.json())
-    .then(({ id, name, types, sprites }: TrimmedPokemonData) => {
-      setCachedObject(`trimmed-data-${pokemonName}`, {
-        id,
-        name,
-        types,
-        sprites,
-      });
-      return { id, name, types, sprites };
-    });
+export const fetchAllPokemonNames = async (): Promise<string[]> => {
+  // return Promise.resolve(['lickitung']);
+  const requestUrl = `${endpoint.url}pokemon/?limit=450`;
+  const jsonResult = await cachedFetchApi<any>(requestUrl);
+  return jsonResult.results
+    .map((pkmn: any) => pkmn.name)
+    .filter((name: string) => name !== 'lickitung');
 };
 
-export const fetchPokemon = ({
+export const fetchPokemon = async ({
   pokemonName,
 }: {
   pokemonName: string;
 }): Promise<FullPokemon> => {
-  const cachedPokemon = getCachedObject<FullPokemon>(pokemonName);
-
-  if (cachedPokemon) return Promise.resolve(cachedPokemon);
-
-  return fetch(`${endpoint.url}pokemon/${pokemonName}`)
-    .then((result) => result.json())
-    .then((jsonResult) => {
-      setCachedObject(pokemonName, jsonResult);
-      return jsonResult;
-    });
+  const requestUrl = `${endpoint.url}pokemon/${pokemonName}`;
+  const jsonResult = await cachedFetchApi<FullPokemon>(requestUrl);
+  return jsonResult;
 };
 
-export const fetchTypes = ({ type }: { type: string }): Promise<PokeType> => {
-  const cachedType = getCachedObject<PokeType>(type);
-
-  if (cachedType) return Promise.resolve(cachedType);
-
-  return fetch(`${endpoint.url}type/${type}`)
-    .then((result) => result.json())
-    .then((jsonResult) => {
-      setCachedObject(type, jsonResult);
-      return jsonResult;
-    });
+export const fetchTypes = async ({
+  type,
+}: {
+  type: string;
+}): Promise<PokeType> => {
+  const requestUrl = `${endpoint.url}type/${type}`;
+  const jsonResult = await cachedFetchApi<PokeType>(requestUrl);
+  return jsonResult;
 };
 
 export const fetchEvolutionData = async ({
@@ -92,21 +72,19 @@ export const fetchEvolutionData = async ({
 }: {
   speciesUrl: string;
 }) => {
-  const speciesData = await fetch(speciesUrl).then(
-    (result) => result.json() as Promise<PokemonSpecies>
+  const speciesData = await cachedFetchApi<PokemonSpecies>(speciesUrl);
+  const evolution = await cachedFetchApi<PokemonEvolutionChain>(
+    speciesData.evolution_chain.url
   );
 
-  // Maybe only look backwards?
-  const evolution = await fetch(speciesData.evolution_chain.url).then(
-    (result) => result.json() as Promise<PokemonEvolutionChain>
-  );
+  if (!evolution) return { evolvesFrom: null };
+
+  const evolutionFrom = await fetchPokemon({
+    pokemonName: speciesData.evolves_from_species.name,
+  });
 
   return {
-    evolvesFrom: speciesData.evolves_from_species
-      ? await fetchPokemon({
-          pokemonName: speciesData.evolves_from_species.name,
-        })
-      : null,
+    evolvesFrom: evolutionFrom,
   };
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import {fetchAllPokemonNames, fetchPokemonForOverview, TrimmedPokemonData } from '../gateways/poke-gateway';
+import {
+  fetchAllPokemonNames,
+  fetchPokemon,
+  TrimmedPokemonData,
+} from '../gateways/poke-gateway';
 import { motion } from 'framer-motion';
 import { PokeOverviewGrid } from '../components/PokeOverviewGrid/PokeOverviewGrid';
 import { SearchInput } from '../components/SearchInput/SearchInput';
 
 export const Home = () => {
-    const [fetchedPokemon, setFetchedPokemon] = useState<TrimmedPokemonData[]>([]);
-    const [filteredPokemon, setFilteredPokemon] = useState<TrimmedPokemonData[]>([]);
+  const [fetchedPokemon, setFetchedPokemon] = useState<TrimmedPokemonData[]>(
+    []
+  );
+  const [filteredPokemon, setFilteredPokemon] = useState<TrimmedPokemonData[]>(
+    []
+  );
 
   useEffect(() => {
-      const fetchAll = async () => {
-          const pkmnNames = await fetchAllPokemonNames();
-          Promise.all(pkmnNames.map((pokemon) => {
-                  return fetchPokemonForOverview({ pokemonName: pokemon });
-              })
-          ).then((result) => {
-              setFetchedPokemon(result);
-              setFilteredPokemon(result);
-          });
-      }
-      fetchAll()
+    const fetchAll = async () => {
+      const pkmnNames = await fetchAllPokemonNames();
+      Promise.all(
+        pkmnNames.map((pokemon) => {
+          return fetchPokemon({ pokemonName: pokemon });
+        })
+      ).then((result) => {
+        setFetchedPokemon(result);
+        setFilteredPokemon(result);
+      });
+    };
+    fetchAll();
   }, []);
 
   // if (loading) <SwordAndShieldLoader />;
 
-
   return (
     <motion.div exit={{ opacity: 0 }}>
-      <SearchInput fetchedPokemon={fetchedPokemon} onChange={(pkmn) => setFilteredPokemon(pkmn)}/>
+      <SearchInput
+        fetchedPokemon={fetchedPokemon}
+        onChange={(pkmn) => setFilteredPokemon(pkmn)}
+      />
       <PokeOverviewGrid pokemons={filteredPokemon} />
     </motion.div>
   );
