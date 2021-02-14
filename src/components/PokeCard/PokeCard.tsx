289639a2e9339b19ce552e2bd40a950ed833382a
Geraldo dos Santos <geraldo.santos@dephion.com>
2021-02-14 20:39:23 +0100

    Remove animation

--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -7,30 +7,11 @@ import type { TrimmedPokemonData } from 'gateways/poke-gateway';
 type PokeCardProps = {
   pokemonData: TrimmedPokemonData;
   onClick: (event: any) => void;
-  custom: any;
-  initial: any;
-  animate: any;
-  variants: any;
 };
 
-export const PokeCard = ({
-  pokemonData,
-  onClick,
-  custom,
-  initial,
-  animate,
-  variants,
-}: PokeCardProps) => {
+export const PokeCard = ({ pokemonData, onClick }: PokeCardProps) => {
   return (
-    <Card
-      custom={custom}
-      initial={initial}
-      animate={animate}
-      variants={variants}
-      id={pokemonData.name}
-      layoutId={pokemonData.name}
-      onClick={onClick}
-    >
+    <Card id={pokemonData.name} layoutId={pokemonData.name} onClick={onClick}>
       <CardHeader>
         #{pokemonData.id} {pokemonData.name}
       </CardHeader>
--- src/components/PokeOverviewGrid/PokeOverviewGrid.tsx
+++ src/components/PokeOverviewGrid/PokeOverviewGrid.tsx
@@ -13,24 +13,21 @@ export const PokeOverviewGrid: React.FC<IPokeOverviewGridProps> = (props) => {
   const history = useHistory();
 
   // https://www.framer.com/api/motion/animation
-  const variants = {
-    backUp: (index: number) => ({
-      y: 0,
-      transition: {
-        delay: index * 0.08,
-      },
-    }),
-    downLow: { y: '10px' },
-  }
+  // TODO - maybe add this somewhere else
+  // const variants = {
+  //   backUp: (index: number) => ({
+  //     y: 0,
+  //     transition: {
+  //       delay: index * 0.08,
+  //     },
+  //   }),
+  //   downLow: { y: '10px' },
+  // }
 
   return (
     <StyledGrid>
       {pokemons.map((data, index) => (
         <PokeCard
-          animate="backUp"
-          initial="downLow"
-          custom={index}
-          variants={variants}
           key={data.name}
           pokemonData={data}
           onClick={(event: any) => {
