7f29d90f9d5b4136f869404db8db8f6fb763a338
Blacksagres <geraldo.nogueirabs@gmail.com>
2021-02-20 17:52:09 +0100

    Prettify the file

--- src/components/SummaryBlock/SummaryLinks.tsx
+++ src/components/SummaryBlock/SummaryLinks.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { HoverBar, StyledSummaryLink, StyledSummaryLinks } from "./SummaryLinks.styles";
+import {
+  HoverBar,
+  StyledSummaryLink,
+  StyledSummaryLinks,
+} from './SummaryLinks.styles';
 
-export const SummaryLinks = (props: { links: string[], currentTab: string, setCurrentTab: (tab: string) => void }) => {
+export const SummaryLinks = (props: {
+  links: string[];
+  currentTab: string;
+  setCurrentTab: (tab: string) => void;
+}) => {
   const { links, setCurrentTab, currentTab } = props;
-  return <StyledSummaryLinks>
-    {links.map(link => (
-      <StyledSummaryLink key={`summary-link-${link}`} isCurrent={link === currentTab} onClick={() => setCurrentTab(link)}>
-        <span>{link}</span>
-        <HoverBar />
-      </StyledSummaryLink>
-    ))}
-  </StyledSummaryLinks>
-}
+  return (
+    <StyledSummaryLinks>
+      {links.map((link) => (
+        <StyledSummaryLink
+          key={`summary-link-${link}`}
+          isCurrent={link === currentTab}
+          onClick={() => setCurrentTab(link)}
+        >
+          <span>{link}</span>
+          <HoverBar />
+        </StyledSummaryLink>
+      ))}
+    </StyledSummaryLinks>
+  );
+};
